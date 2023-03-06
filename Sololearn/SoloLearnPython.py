ea32e1f4abff2389be404441816d249b8a201c21
Answed <schradearwed@gmail.com>
2023-03-06 15:47:18 +0100

    Added answer from juice maker

--- Sololearn/SoloLearnPython.py
+++ Sololearn/SoloLearnPython.py
@@ -20,3 +20,25 @@ def fibonacci(n):
 #Prints the results from 0 to num
 for i in range(num):
 	print(fibonacci(i))
+	
+#Answe to Juice Maker from the Sololearn Python Course
+class Juice:
+    def __init__(self, name, capacity):
+        self.name = name
+        self.capacity = capacity
+
+    def __add__(self, other):
+        self.name = self.name + "&" + other.name
+        self.capacity = self.capacity + other.capacity
+        return self
+
+    def __str__(self):
+        return (self.name + ' ('+str(self.capacity)+'L)')
+
+
+
+a = Juice('Orange', 1.5)
+b = Juice('Apple', 2.0)
+
+result = a + b
+print(result)
