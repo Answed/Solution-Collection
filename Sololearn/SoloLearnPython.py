9067b846b49009c67abf3af45c81b58d776bf89e
Answed <schradearwed@gmail.com>
2023-03-20 12:45:33 +0100

    Added solotion for House Prices

--- Sololearn/SoloLearnPython.py
+++ Sololearn/SoloLearnPython.py
@@ -75,3 +75,13 @@ for player in players:
         player_in_range += 1
 
 print(player_in_range)
+
+# Answe to House Prices from the Python for Data Science Course
+data = np.array([150000, 125000, 320000, 540000, 200000, 120000, 160000, 230000, 280000, 290000, 300000, 500000, 420000, 100000, 150000, 280000])
+
+standard_dev = np.std(data)
+mean = np.mean(data)
+
+result = data[(data > mean-standard_dev) & (data < mean + standard_dev)]
+
+print((len(result) / len(data)) * 100)
