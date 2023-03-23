43a1c15483e5332881a3d59dcada7c20c3edfa75
Answed <schradearwed@gmail.com>
2023-03-23 18:45:40 +0100

    Added Solotion for Covid Data Analysis

--- Sololearn/SoloLearnPython.py
+++ Sololearn/SoloLearnPython.py
@@ -85,3 +85,15 @@ mean = np.mean(data)
 result = data[(data > mean-standard_dev) & (data < mean + standard_dev)]
 
 print((len(result) / len(data)) * 100)
+
+# Annswer to COVID Data Analysis from the Python for Data Science Course
+import pandas as pd
+
+df = pd.read_csv("/usercode/files/ca-covid.csv")
+
+df.drop('state', axis=1, inplace=True)
+df.set_index('date', inplace=True)
+
+df['ratio'] = df['deaths'] / df['cases']
+
+print(df.loc[df['ratio'] == df['ratio'].max() ])
