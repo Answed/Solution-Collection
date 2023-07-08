49edf27f0713c520510fcd92ae4369a9444b59d4
Answed <schradearwed@gmail.com>
2023-07-08 19:12:53 +0200

    Solution for arithmetic

---
+++ CodeWars/arithmetics.py
@@ -0,0 +1,8 @@
+def arithmetic(a, b, operator):
+    if(operator == "add"):
+        return a+b
+    elif(operator == "subtract"):
+        return a-b
+    elif(operator == "multiply"):
+        return a*b
+    else: return a/b
