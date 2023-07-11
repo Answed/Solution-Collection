e2f6f3a1012f5ad6c2ee6ff9bde2bf079e929b8b
Answed <schradearwed@gmail.com>
2023-07-11 12:12:14 +0200

    Solution for Rot13

---
+++ CodeWars/Rot13.py
@@ -0,0 +1,16 @@
+import string
+def rot13(message):
+    new_message = ""
+    for char in message:
+        if(char.isalpha()):
+            new_char = (find_alphabet_position(char) + 13) % 26
+            if(char.isupper()):
+                new_message += string.ascii_uppercase[new_char]
+            else: new_message += string.ascii_lowercase[new_char]
+        else: new_message += char
+    return new_message
+
+def find_alphabet_position(char):
+    return string.ascii_lowercase.index(char.lower())
+
+print(rot13("test"))
