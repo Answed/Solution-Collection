21e7c6ff7dfd3ca0ce2c6af8969b2eadc8caa054
Answed <schradearwed@gmail.com>
2023-08-11 11:43:32 +0200

    Added Solution for Stack

--- Sololearn/SoloLearnPython.py
+++ Sololearn/SoloLearnPython.py
@@ -191,6 +191,29 @@ new_income = calculateIncome(age, data)
 grwoth = ((new_income - base_income)/base_income)*100
 print(int(grwoth))
 
+class Browser:
+    def __init__(self):
+      self.links = []  
+  
+    def is_empty(self):
+      return self.links == []
+  
+    def push(self, link):
+      self.links.insert(0, link)
+    def pop(self):
+      return self.links.pop(0)
+    
+    
+  
+x = Browser()
+x.push('about:blank')
+x.push('www.sololearn.com')
+x.push('www.sololearn.com/courses/')
+x.push('www.sololearn.com/courses/python/')
+
+while not x.is_empty():
+    print(x.pop())
+
 class CallCenter:
     def __init__(self):
       self.customers = []
