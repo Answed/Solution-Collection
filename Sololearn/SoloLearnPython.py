752a91e38bfb32a4e5664e3ee91ba9ce6edae358
Answed <schradearwed@gmail.com>
2023-08-12 11:01:59 +0200

    Added Solution for graphs

--- Sololearn/SoloLearnPython.py
+++ Sololearn/SoloLearnPython.py
@@ -282,3 +282,41 @@ while True:
 
 #your code goes here
 p.play()
+
+class X(): 
+    def __init__(self, size): 
+        self.adj = [ [0] * size for i in range(size)]
+        self.size = size 
+    
+    def add_friend(self, x, y): 
+        if x > self.size or y > self.size or x < 0 or y < 0: 
+            print("Error") 
+        else: 
+            self.adj[x-1][y-1] = 1 
+            self.adj[y-1][x-1] = 1 
+        
+    def remove_friend(self, x, y): 
+        if x > self.size or y > self.size or x < 0 or y < 0: 
+            print("Error") 
+        else: 
+            self.adj[x-1][y-1] = 0 
+            self.adj[y-1][x-1] = 0 
+            
+
+x = X(5)
+
+x.add_friend(1, 3) 
+x.add_friend(1, 5)
+x.add_friend(2, 5)
+x.add_friend(2, 4)
+x.add_friend(4, 5)
+
+n = int(input())
+#your code goes here
+connections = 0
+
+for connection in range(5):
+    if(x.adj[n-1][connection-1] == 1):
+        print(x.adj[n-1][connection-1])
+        connections += 1
+print(connections)
