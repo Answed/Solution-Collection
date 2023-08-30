502d622bdd848b7e34e47f64a083131f8ddef948
Answed <schradearwed@gmail.com>
2023-08-30 09:49:05 +0200

    Added Solution for Only the Evens !

--- Sololearn/SoloLearnCSharp.cs
+++ Sololearn/SoloLearnCSharp.cs
@@ -111,3 +111,30 @@ namespace Code_Coach_Challenge
             catch { Console.WriteLine("Invalid "); }
         }
     }
+
+    namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+            int count = 0;
+            while (count <5)
+            {
+                numbers[count] = Convert.ToInt32(Console.ReadLine());
+                count++;
+            }
+            //your code goes here
+            
+            var sumOfEven = 0;
+
+            foreach(var num in numbers){
+                if(num%2 == 0)
+                    sumOfEven += num;
+            }
+
+            Console.WriteLine(sumOfEven);
+        }
+    }
+}
