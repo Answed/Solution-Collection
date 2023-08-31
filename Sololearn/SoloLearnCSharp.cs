de1bf53b8d8c0f9bb5200c8349ca9db751091130
Answed <schradearwed@gmail.com>
2023-08-31 12:11:14 +0200

    Added Solution for Words

--- Sololearn/SoloLearnCSharp.cs
+++ Sololearn/SoloLearnCSharp.cs
@@ -206,3 +206,40 @@ namespace SoloLearn
         }
     }
 }
+
+namespace Code_Coach_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = {
+                "home",
+                "programming",
+                "victory",
+                "C#",
+                "football",
+                "sport",
+                "book",
+                "learn",
+                "dream",
+                "fun"
+            };
+
+            string letter = Console.ReadLine();
+            int count = 0;
+
+            foreach (string word in words)
+            {
+                if (word.Contains(letter))
+                {
+                    count++;
+                    Console.WriteLine(word);
+                }
+            }
+
+            if (count == 0)
+                Console.WriteLine("No match");
+        }
+    }
+}
