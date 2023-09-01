0f72b1e2284b915e4ec598168de5666db4bf9cbb
Answed <schradearwed@gmail.com>
2023-09-01 18:27:04 +0200

    Solution for who won

--- Sololearn/SoloLearnCSharp.cs
+++ Sololearn/SoloLearnCSharp.cs
@@ -242,4 +242,32 @@ namespace Code_Coach_Challenge
                 Console.WriteLine("No match");
         }
     }
+
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] finalists = { "James Van", "John Smith", "Leyla Brown", "Tom Homerton", "Bob Douglas" };
+
+            int winner = Convert.ToInt32(Console.ReadLine());
+
+            //this should show the winner and "Game Over"
+            FinalRound finalRound = new FinalRound(finalists, winner);
+        }
+    }
+
+    class FinalRound
+    {
+        public FinalRound(string[] finalists, int winner)
+        {
+            //complete the constructor
+            Console.WriteLine("Winner is " + finalists [winner ]);
+        }
+        
+        //create destructor => "Game Over"
+        ~FinalRound(){
+            Console.WriteLine("Game Over");
+        }
+        
+    }
 }
