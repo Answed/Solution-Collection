84075bca6080ed3d9beb71457d5836e06f275880
Answed <schradearwed@gmail.com>
2023-09-01 18:28:05 +0200

    Solution for grow your buisness

--- Sololearn/SoloLearnCSharp.cs
+++ Sololearn/SoloLearnCSharp.cs
@@ -270,4 +270,38 @@ namespace Code_Coach_Challenge
         }
         
     }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfDeps = Convert.ToInt32(Console.ReadLine());
+
+            int count = 0;
+            while (count < numOfDeps)
+            {
+                string depName = Console.ReadLine();
+                Department dep = new Department(depName);
+                count++;
+            }
+
+            Console.WriteLine("Number of departments: " + Department.depCount);
+        }
+    }
+    class Department
+    {
+        
+        public string depName;
+        
+        public static int depCount = 2;
+
+        //declare static depCount member with value of 2
+
+        //complete the constructor
+        public Department(string name)
+        {
+            Console.WriteLine("Department opened: " + name);
+            this.depName = name;
+            depCount++;
+        }
+    }
 }
