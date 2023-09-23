03fc26798842b29c8978f7a1a5878ece6860d2d7
Answed <schradearwed@gmail.com>
2023-09-23 15:22:21 +0200

    Added Solution for Teamwork Makes the Dream Work

--- Sololearn/SoloLearnCSharp.cs
+++ Sololearn/SoloLearnCSharp.cs
@@ -390,4 +390,34 @@ namespace Code_Coach_Challenge
         }
         
     }
+        class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Score tm1 = new Score(2, 3);
+            Score tm2 = new Score(4, 2);
+
+            Score finalScores = tm1 + tm2;
+
+
+            Console.WriteLine("Round 1: " + finalScores.round1Score);
+            Console.WriteLine("Round 2: " + finalScores.round2Score);
+        }
+    }
+    class Score
+    {
+        public int round1Score { get; set; }
+        public int round2Score { get; set; }
+        public Score(int r1, int r2)
+        {
+            round1Score = r1;
+            round2Score = r2;
+        }
+        
+        //your code goes here
+        public static Score operator+(Score tm1, Score tm2){
+            return new Score(tm1.round1Score + tm2.round1Score, tm1.round2Score + tm2.round2Score);
+        }
+    }
 }
