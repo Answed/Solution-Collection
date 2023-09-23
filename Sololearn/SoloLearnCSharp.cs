3a361e07e1226d865df653033d63cf22e4fa52e5
Answed <schradearwed@gmail.com>
2023-09-23 15:01:07 +0200

    Added Solution for Sorting Arrays

--- Sololearn/SoloLearnCSharp.cs
+++ Sololearn/SoloLearnCSharp.cs
@@ -304,4 +304,26 @@ namespace Code_Coach_Challenge
             depCount++;
         }
     }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            int[] numbers = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Array.Sort(numbers);
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+        }
+    }
 }
