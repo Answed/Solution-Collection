6ec9cefd2e4f130d51e9f27ed4c61259b9a171b1
Answed <schradearwed@gmail.com>
2023-09-24 15:53:41 +0200

    Added Solution for Whats My Account Balance

--- Sololearn/SoloLearnCSharp.cs
+++ Sololearn/SoloLearnCSharp.cs
@@ -493,5 +493,42 @@ namespace Code_Coach_Challenge
         }
         
     }
+
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            string accNumber = Console.ReadLine();
+            double balance = Convert.ToDouble(Console.ReadLine());
+
+            User user = new User(accNumber, balance);
+
+            user.ShowDetails();
+
+        }
+    }
+
+    class Account
+    {
+        protected double Balance { get; set; }
+    }
+
+    class User : Account
+    {
+        public string AccNumber { get; set; }
+        
+        //complete the constructor
+        public User(string accNumber, double balance)
+        {
+            this.AccNumber = accNumber;
+            this.Balance = balance;
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine("Account N: " + AccNumber);
+            Console.WriteLine("Balance: " + Balance);
+        }
+    }
 }
 }
