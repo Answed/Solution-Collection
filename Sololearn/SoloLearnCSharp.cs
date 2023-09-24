710bd88a7e3cf55297db55fee0f9046eb21c6456
Answed <schradearwed@gmail.com>
2023-09-24 15:50:33 +0200

    Added Solution for Make And Model

--- Sololearn/SoloLearnCSharp.cs
+++ Sololearn/SoloLearnCSharp.cs
@@ -456,4 +456,42 @@ namespace Code_Coach_Challenge
             return new DancerPoints(tempName, tempPoints);
         }
     }
+    namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string brandName = Console.ReadLine();
+            string modelName = Console.ReadLine();
+
+            Car car = new Car();
+            car.Brand = brandName;
+            car.Model = modelName;
+
+            car.ShowBrand();
+            car.ShowModel();
+        }
+    }
+    class Vehicle
+    {
+        public string Brand { get; set; }
+
+        public void ShowBrand()
+        {
+            Console.WriteLine("Brand: " + Brand);
+        }
+    }
+    
+    //complete the Car class
+    class Car : Vehicle
+    {
+        public string Model { get; set; }
+
+        public void ShowModel(){
+            Console.WriteLine("Model: " + Model);
+        }
+        
+    }
+}
 }
