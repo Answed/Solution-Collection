4bad98a19dfd3220a58a4039dfce76b76578a65a
Answed <schradearwed@gmail.com>
2023-09-30 16:43:14 +0200

    Added Solution fo rAttack!

--- Sololearn/SoloLearnCSharp.cs
+++ Sololearn/SoloLearnCSharp.cs
@@ -530,5 +530,43 @@ namespace Code_Coach_Challenge
             Console.WriteLine("Balance: " + Balance);
         }
     }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            Unit unit1 = new Unit();
+            Unit musketeer = new Musketeer();
+            Unit magician = new Magician();
+
+            unit1.Attack();
+            musketeer.Attack();
+            magician.Attack();
+        }
+    }
+
+    class Unit
+    {
+        public virtual void Attack()
+        {
+            Console.WriteLine("Using sword!");
+        }
+    }
+    
+    /*derive the class from Unit class
+    and override Attack() method*/
+    class Musketeer : Unit
+    {
+        public override void Attack(){
+            Console.WriteLine("Using musket!");
+        }
+    }
+    /*derive the class from Unit class
+    and override Attack() method*/
+    class Magician : Unit
+    {
+        public override void Attack(){
+            Console.WriteLine("Using magic!");
+        }
+    }
 }
 }
