e878d58ea8ec2ff32713f87769ed84421bcafeef
Answed <schradearwed@gmail.com>
2023-09-30 16:46:44 +0200

    Added Solution for Drawing Applicaton

--- Sololearn/SoloLearnCSharp.cs
+++ Sololearn/SoloLearnCSharp.cs
@@ -570,3 +570,50 @@ namespace Code_Coach_Challenge
     }
 }
 }
+namespace Code_Coach_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Draw pencil = new Draw();
+            Draw brush = new Brush();
+            Draw spray = new Spray();
+
+            pencil.StartDraw();
+            brush.StartDraw();
+            spray.StartDraw();
+
+        }
+    }
+
+    public interface IDraw
+    {
+        void StartDraw();
+    }
+
+    class Draw : IDraw
+    {
+        public virtual void StartDraw()
+        {
+            Console.WriteLine("Using pencil");
+        }
+    }
+
+    //inherit this class from the class Draw
+    class Brush : Draw
+    {
+        public override void StartDraw(){
+            Console.WriteLine("Using brush");
+        }
+    }
+
+    //inherit this class from the class Draw
+    class Spray : Draw
+    {
+            public override void StartDraw(){
+            Console.WriteLine("Using spray");
+        }
+
+    }
+}
