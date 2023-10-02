19da94ce8e0703d945b74e3bc94ee021c0e93352
Answed <schradearwed@gmail.com>
2023-10-02 08:38:57 +0200

    Added Solution for Robot-Barmen

--- Sololearn/SoloLearnCSharp.cs
+++ Sololearn/SoloLearnCSharp.cs
@@ -712,4 +712,31 @@ namespace Code_Coach_Challenge
             }
         }
     }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int drinks = Convert.ToInt32(Console.ReadLine());
+                int shelves = Convert.ToInt32(Console.ReadLine());
+
+                //your code goes here
+                Console.WriteLine(drinks/shelves);
+
+            }
+            /*
+             * 1. DivideByZeroException => "At least 1 shelf"
+             * 2. FormatException => "Please insert an integer"
+            */
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("At least 1 shelf");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Please insert an integer");
+            }
+        }
+    }
 }
