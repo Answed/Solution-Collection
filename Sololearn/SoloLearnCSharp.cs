852fef85483abf83909c82f87829c9e63f258560
Answed <schradearwed@gmail.com>
2023-10-02 08:06:21 +0200

    Added Solution for Accelerate

--- Sololearn/SoloLearnCSharp.cs
+++ Sololearn/SoloLearnCSharp.cs
@@ -655,4 +655,46 @@ namespace Code_Coach_Challenge
             return 4*(length+width+height);
         }
     }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Player player1 = new Player(Difficulty.Easy);
+            Player player2 = new Player(Difficulty.Medium);
+            Player player3 = new Player(Difficulty.Hard);
+        }
+    }
+    
+    /*
+    Easy => "You have 3 minutes 45 seconds"
+    Medium = > "You have 3 minutes 20 seconds"
+    Hard => "You have 3 minutes"
+    */
+
+    class Player
+    {
+        public Player(Difficulty x)
+        {
+            switch(x)
+            {
+                case Difficulty.Easy:
+                    Console.WriteLine("You have 3 minutes 45 seconds");
+                    break;
+                case Difficulty.Medium:
+                    Console.WriteLine("You have 3 minutes 20 seconds");
+                    break;
+                case Difficulty.Hard:
+                    Console.WriteLine("You have 3 minutes");
+                    break;
+            }
+            
+        }
+    }
+     enum Difficulty
+    {
+        Easy,
+        Medium,
+        Hard
+    };
 }
