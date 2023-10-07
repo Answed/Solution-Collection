d0ff389714320e4e89e63d7caf44d73624ef2c3a
Answed <schradearwed@gmail.com>
2023-10-07 10:29:07 +0200

    Added Solution for Hiring Engineers

--- Sololearn/SoloLearnCSharp.cs
+++ Sololearn/SoloLearnCSharp.cs
@@ -819,4 +819,35 @@ namespace Code_Coach_Challenge
             }
         }
     }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> candidates = new HashSet<string>();
+
+            candidates.Add("John");
+            candidates.Add("Amelie");
+            candidates.Add("Tom");
+            candidates.Add("Richard");
+            candidates.Add("Barbara");
+            candidates.Add("Susan");
+            candidates.Add("Charles");
+            candidates.Add("Daniel");
+            candidates.Add("Tamara");
+            candidates.Add("Donald");
+
+            HashSet<string> hiring = new HashSet<string>();
+
+            while (hiring.Count<3)
+            {
+                string hire = Console.ReadLine();
+                hiring.Add(hire);
+                
+            }
+            if(hiring.IsSubsetOf(candidates))
+                Console.WriteLine("Starting hiring process");
+            else Console.WriteLine("Something is wrong");
+            
+        }
+    }
 }
