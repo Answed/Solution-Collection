519125134d685c6ba5d0ef80f3df9b2fef57cce5
Answed <schradearwed@gmail.com>
2023-10-23 18:29:17 +0200

    Added Solution for Preservation

--- Sololearn/SoloLearnPython.py
+++ Sololearn/SoloLearnPython.py
@@ -578,3 +578,18 @@ result = s1 + s2
 
 print(result.area())
 print(s1 > s2)
+
+class Player:
+    def __init__(self, name, lives):
+        self.name = name
+        self._lives = lives
+
+    def hit(self):
+        self._lives -= 1
+        if(self._lives <= 0): print("Game Over")
+        
+
+p = Player("Cyberpunk77", 3)
+p.hit()
+p.hit()
+p.hit()
